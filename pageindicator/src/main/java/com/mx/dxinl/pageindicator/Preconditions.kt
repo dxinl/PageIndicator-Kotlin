f3795eeb9e768ef1f528fa1bec8a141c36926402
Xinliang <907667336@qq.com>
2017-07-30 20:52:40 +0800

    Modify annotation

--- pageindicator/src/main/java/com/mx/dxinl/pageindicator/Preconditions.kt
+++ pageindicator/src/main/java/com/mx/dxinl/pageindicator/Preconditions.kt
@@ -1,7 +1,7 @@
 package com.mx.dxinl.pageindicator
 
 /**
- * Created by dxinl on 2017/07/30.
+ * Fail-fast
  */
 fun checkNotNull(any: Any?, argumentName: String) {
     any ?: throw IllegalArgumentException(argumentName + " cannot be null")
